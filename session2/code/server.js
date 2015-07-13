b0635672645c2c101ae43b6a3f245140bc802474
Zed A. Shaw <zed@mesosphere.io>
2015-07-13 10:59:23 -0700

    Needed for the second session.

---
+++ session2/code/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+
+var www = http.createServer(handleRequest);
+www.listen(8080);
